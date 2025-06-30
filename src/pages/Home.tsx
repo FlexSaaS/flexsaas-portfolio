627bea707ca31aa70d59327354e23086335decfa
Pierre Guichard <117652420+pierrelouisguichard@users.noreply.github.com>
2025-06-30 14:40:15 +0100

    Added Background scrolling gallery.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,16 @@
-// import Features from "../components/common/FeatureHighlight";
 import EmailForm from "../components/common/ContactUs";
 import Hero from "../components/common/Hero";
 import Pricing from "../components/common/Pricing";
 import Projects from "../components/common/Projects";
-import Services from "../components/common/Services";
 import Team from "../components/common/Team";
 import Testimonials from "../components/common/Testimonials";
+import InfiniteImageScroll from "../components/common/WebsiteGallery";
 
 function Home() {
   return (
     <>
       <Hero />
-      <Services />
       <Projects />
-      {/* <Features /> */}
       <Testimonials />
       <Pricing />
       <Team />
