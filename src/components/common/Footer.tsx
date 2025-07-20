c10f2771a74e562e90c31cceaf675c993d23ea47
safo samson <safosamson18@gmail.com>
2025-07-20 16:11:08 +0100

    minor fixes

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,4 +1,7 @@
-import styled from 'styled-components';
+import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 
 function Footer() {
   return (
@@ -7,47 +10,77 @@ function Footer() {
         <FooterColumn>
           <h3>Services</h3>
           <FooterLinks>
-            <FooterLink><a href="#services">Construction Portfolios</a></FooterLink>
-            <FooterLink><a href="#services">Beauty Booking Systems</a></FooterLink>
-            <FooterLink><a href="#services">Custom Solutions</a></FooterLink>
+            <FooterLink>
+              <a href="#services">Construction Portfolios</a>
+            </FooterLink>
+            <FooterLink>
+              <a href="#services">Beauty Booking Systems</a>
+            </FooterLink>
+            <FooterLink>
+              <a href="#services">Custom Solutions</a>
+            </FooterLink>
           </FooterLinks>
         </FooterColumn>
-        
+
         <FooterColumn>
           <h3>Company</h3>
           <FooterLinks>
-            <FooterLink><a href="#team">About Us</a></FooterLink>
-            <FooterLink><a href="#team">Our Team</a></FooterLink>
+            <FooterLink>
+              <a href="#team">About Us</a>
+            </FooterLink>
+            <FooterLink>
+              <a href="#team">Our Team</a>
+            </FooterLink>
             {/* <FooterLink><a href="#">Careers</a></FooterLink> */}
-            <FooterLink><a href="#contact">Contact</a></FooterLink>
+            <FooterLink>
+              <a href="#contact">Contact</a>
+            </FooterLink>
           </FooterLinks>
         </FooterColumn>
-        
+
         <FooterColumn>
           <h3>Resources</h3>
           <FooterLinks>
-            <FooterLink><a href="#">Blog</a></FooterLink>
-            <FooterLink><a href="#contact">Help Center</a></FooterLink>
+            <FooterLink>
+              <a href="#">Blog</a>
+            </FooterLink>
+            <FooterLink>
+              <a href="#contact">Help Center</a>
+            </FooterLink>
           </FooterLinks>
         </FooterColumn>
-        
+
         <FooterColumn>
           <h3>Legal</h3>
           <FooterLinks>
-            <FooterLink><a href="#">Privacy Policy</a></FooterLink>
-            <FooterLink><a href="#">Terms of Service</a></FooterLink>
-            <FooterLink><a href="#">Cookie Policy</a></FooterLink>
+            <FooterLink>
+              <a href="#">Privacy Policy</a>
+            </FooterLink>
+            <FooterLink>
+              <a href="#">Terms of Service</a>
+            </FooterLink>
+            <FooterLink>
+              <a href="#">Cookie Policy</a>
+            </FooterLink>
           </FooterLinks>
         </FooterColumn>
       </FooterGrid>
-      
+
       <FooterBottom>
-        <div>© {new Date().getFullYear()} FlexSaaS. All rights reserved.</div>
+        <div>© {new Date().getFullYear()} FlexSaaS. </div>
         <SocialLinks aria-label="Social media links">
-          <a href="https://twitter.com/flexsaas" aria-label="FlexSaaS on Twitter"><i className="fab fa-twitter" aria-hidden="true"></i></a>
-          <a href="https://facebook.com/flexsaas" aria-label="FlexSaaS on Facebook"><i className="fab fa-facebook" aria-hidden="true"></i></a>
-          <a href="https://instagram.com/flexsaas" aria-label="FlexSaaS on Instagram"><i className="fab fa-instagram" aria-hidden="true"></i></a>
-          <a href="https://linkedin.com/company/flexsaas" aria-label="FlexSaaS on LinkedIn"><i className="fab fa-linkedin" aria-hidden="true"></i></a>
+          <a href="https://twitter.com/flexsaas" aria-label="FlexSaaS on Twitter">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href="https://facebook.com/flexsaas" aria-label="FlexSaaS on Facebook">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="https://instagram.com/flexsaas" aria-label="FlexSaaS on Instagram">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="https://linkedin.com/company/flexsaas" aria-label="FlexSaaS on LinkedIn">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
         </SocialLinks>
       </FooterBottom>
     </FooterContainer>
@@ -60,8 +93,48 @@ const FooterContainer = styled.footer`
   background: #333;
   color: white;
   padding: 5rem 10% 2rem;
+  min-height: 400px; /* Ensure footer has minimum height */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
+const FooterBottom = styled.div`
+  border-top: 1px solid #444;
+  padding-top: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #999;
+  font-size: 0.9rem;
+  margin-top: auto; /* Pushes it to the bottom */
+`;
+
+const SocialLinks = styled.div`
+  display: flex;
+  display: none; /* Hide by default for now till we get social media links */
+  gap: 1.5rem;
+
+  a {
+    color: #bbb;
+    font-size: 1.2rem;
+    transition: color 0.3s;
+    width: 24px;
+    height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+      color: white;
+    }
+
+    svg {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
 const FooterGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
@@ -85,37 +158,12 @@ const FooterLinks = styled.ul`
 
 const FooterLink = styled.li`
   margin-bottom: 0.8rem;
-  
+
   a {
     color: #bbb;
     text-decoration: none;
     transition: color 0.3s;
-    
-    &:hover {
-      color: white;
-    }
-  }
-`;
 
-const FooterBottom = styled.div`
-  border-top: 1px solid #444;
-  padding-top: 2rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: #999;
-  font-size: 0.9rem;
-`;
-
-const SocialLinks = styled.div`
-  display: flex;
-  gap: 1rem;
-  
-  a {
-    color: #bbb;
-    font-size: 1.2rem;
-    transition: color 0.3s;
-    
     &:hover {
       color: white;
     }
