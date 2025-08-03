735988c07659049f5da8f28e82f55ded10871c28
Pierre Guichard <117652420+pierrelouisguichard@users.noreply.github.com>
2025-08-03 13:35:08 +0100

    Update ContactUs.tsx

--- src/components/common/ContactUs.tsx
+++ src/components/common/ContactUs.tsx
@@ -39,11 +39,11 @@ function EmailForm() {
       });
   };
 
-  const handleWhatsAppClick = () => {
-    const phoneNumber = "+447352467408";
-    const message = encodeURIComponent("Hi Flexsaas, I am interested in your services.");
-    window.open(`https://wa.me/${phoneNumber}text=${message}`, "_blank");
-  };
+const handleWhatsAppClick = () => {
+  const phoneNumber = "447352467408"; 
+  const message = encodeURIComponent("Hi Flexsaas, I am interested in your services.");
+  window.open(`https://wa.me/${phoneNumber}?text=${message}`, "_blank");
+};
 
   const handleEmailClick = () => {
     window.location.href = "mailto:support@flexsaas.co.uk";
